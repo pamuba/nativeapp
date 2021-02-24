aa77319eb9ebee3d95cf6ab5ba9f330e24897ab5
pamuba <partha.bora24@gmail.com>
2021-02-24 22:44:03 +0530

    355

--- App.js
+++ App.js
@@ -5,8 +5,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello World</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>Hello How RU?</Text>
+        <Text>Hello How RU?</Text>
+        <Text>Hello How RU?</Text>
+        <Text>Hello How RU?</Text>
+      </View>
     </View>
   );
 }
@@ -18,4 +25,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    backgroundColor:'pink',
+    padding:20,
+  },
+  boldText:{
+    fontWeight:'bold'
+  },
+  body:{
+    backgroundColor:'yellow',
+    padding:20
+  }
 });
